c6c140e4dac659acdbb25c90d2647af3fe70547b
bismarck <bismarquemomanyi@gmail.com>
2022-10-16 01:07:02 -0700

    for loop

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point to a program
+ *Return: 0 for succiss
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ *main - Entry point of a program
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 26; ++c)
+		putchar('a' + c);
+	putchar('\n');
+
+	return (0);
+}
