3b04e2c04a30eb0f39c38c0dee102c05023d5ac2
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 05:38:35 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = "a"; alphabet <= "z"; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+}
