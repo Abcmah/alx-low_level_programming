7e8843b104e1dba8eba0afc97d87934abe220901
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 07:30:28 -0700

    s

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+/**
+ *print_last_digit - Returns last digit of a givin output
+ *@n: Holds the value passed
+ *Return: last digt of n
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last n % 10;
+
+	return (last);
+}
