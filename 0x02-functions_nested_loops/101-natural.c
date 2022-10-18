2f1d2c33e8c500d51be7e939bc6e8aedb0da5baa
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 08:31:08 -0700

    s

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - checks  multiples of 3 and 5
+ *
+ * Return: 0 always (suc)
+ */
+int main(void)
+{
+int a = 1024;
+int b=0; 
+int sum = 0;
+
+
+for (b = 0; b < a; b++)
+{
+if ((b % 3 == 0) || (b % 5 == 0))
+{
+sum = sum + b;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
