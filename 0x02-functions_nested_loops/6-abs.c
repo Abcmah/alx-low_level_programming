0beb3b27cb4f3fa9486a4f315b9d967c49625c59
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 07:17:37 -0700

    s

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ *_abs - Returns an absolute value
+ *
+ *@n: Holds value passed to the fun
+ *Return: Absolute value
+ */
+int _abs(int n)
+{
+	int r;
+
+	r = abs(n);
+	return (r);
+}
