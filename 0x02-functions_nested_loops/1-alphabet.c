1c3da643e8d7a4e9ea0fb40324c2b249b3c3980a
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 05:43:13 -0700

    al"

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
 
-	return (0);
-}
+/**
+ *print_alphabet - Prints lowercase alphabets
+ */
 void print_alphabet(void)
 {
 	char alphabet;
