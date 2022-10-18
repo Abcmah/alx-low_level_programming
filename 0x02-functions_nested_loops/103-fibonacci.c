f11559f6d87e5a9de9f2762585500d9af5f5e8a3
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 08:52:25 -0700

    fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 unsigned long int lim = 4000000;
-unsigned long int befere = 0;
+unsigned long int before = 0;
 unsigned long int after = 1;
 unsigned long int x = 0;
 unsigned long int sum = 0;
