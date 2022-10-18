feca955e1c1802440ae623dde4a5252c0dfbc3ce
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 07:51:20 -0700

    ch

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,16 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = n % 10;
-	_putchar(last + '0');
-
-	return (last);
+	if (n < 0)
+	{
+		last = (-1 * (n % 10));
+		_putchar(last + '0');
+		return (last);
+	}
+	else
+	{
+		last = n % 10;
+		_putchar(last + '0')
+		return (last);
+	}
 }
