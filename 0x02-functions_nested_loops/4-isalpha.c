cd0bea2b41b237b82a0aff0e7f8f5c87febe8bb0
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 06:43:33 -0700

    ard

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+/**
+ *_isalpha - Returns 1 if is a letter otherwise 0
+ *@c: holds value passed
+ *Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
