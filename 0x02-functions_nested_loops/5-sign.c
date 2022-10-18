d42ca119622848b28b415cf5305b897ea4d91513
bismarck <bismarquemomanyi@gmail.com>
2022-10-18 07:00:27 -0700

    w

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_sign - Prints and return either 0,1,-1
+ *@n: Holds value passed
+ *Return: 0, 1 ,-1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else(n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
