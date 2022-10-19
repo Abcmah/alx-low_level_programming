8caf887b574ad45f969d1f0b29159df448543e87
bismarck <bismarquemomanyi@gmail.com>
2022-10-19 04:02:31 -0700

    11 - 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,11 +6,20 @@
  */
 int main(void)
 {
-	char n;
+	unsigned char a = '0';
+	int i;
 
-	for (n = '0'; n <= '9'; ++n)
-		putchar(n);
-	for (n = 'a'; n <= 'f'; ++n)
-		putchar(n);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
+		putchar('0' + a);
+		a++;
+	}
 	putchar('\n');
+	return (0);
 }
