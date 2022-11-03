157d224e10a0732dcea5b5d1d13f20ab90449e74
bismarck <bismarquemomanyi@gmail.com>
2022-11-03 05:34:18 -0700

    test 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <math.h>
+/**
+ *_sqrt_recursion - Return squareroot
+ *@n: holds a number to be sqted
+ *Return: str
+ */
+
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n));
+}
