f9797acaffb0d25636e72672ca78408d97fc4ec7
bismarck <bismarquemomanyi@gmail.com>
2022-11-03 04:32:40 -0700

    test 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
+#include <string.h>
 
+/**
+ *_strlen_recursion - return the length of a string
+ *@s: Passed string
+ *Return: the len of s
+ */
 int _strlen_recursion(char *s)
 {
 	return (strlen(s));
