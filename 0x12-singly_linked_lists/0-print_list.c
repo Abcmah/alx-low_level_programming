2ff1da65d4c6275481756a125c94fd34454a8064
bismarck <bismarquemomanyi@gmail.com>
2022-11-28 10:49:46 -0800

    fisrt

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,18 @@
 #include "lists.h"
-
+/**
+ * print_list - prints the member
+ * @h: node
+ * Return: size of list
+ */
 size_t print_list(const list_t *h)
 {
 	int size = 0;
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n",(h->str == NULL ? 0 : h->len),(h->str != NULL ? h->str : "(nil)"));
+		printf("[%d] %s\n", (h->str == NULL ? 0 : h->len), (h->str != NULL ? h->str : "(nil)"));
 		h = h->next;
-		size ++;
+		size++;
 	}
 	return (size);
 }
